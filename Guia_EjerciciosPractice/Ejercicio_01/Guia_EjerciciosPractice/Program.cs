00bddb9fb0ca2041efb1477b54bd8c5c34eddf4c
Ludmila Cisneros <ludmilacisneros97@hotmail.com>
2020-08-30 18:57:48 -0300

    EJ 1Y2 OK

---
+++ Guia_EjerciciosPractice/Ejercicio_01/Guia_EjerciciosPractice/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_EjerciciosPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 01";
+            int numero;
+            int acumulador = 0;
+            int numeroMayor = 0;
+            int numeroMenor = 0;
+            float promedio = 0;
+
+            for(int i=0;i<5; i++)
+            {
+                Console.Write("\nIngrese un numero: ");
+                numero = int.Parse(Console.ReadLine());
+
+                acumulador += numero;
+
+                if(i==0 || numero > numeroMayor)
+                {
+                    numeroMayor = numero;
+                }
+                if(i==0 || numero < numeroMenor)
+                {
+                    numeroMenor = numero;
+                }
+
+            }
+            promedio = (float)acumulador / 5;
+            Console.WriteLine("Mayor: {0}",numeroMayor);
+            Console.WriteLine("Menor: {0}",numeroMenor);
+            Console.WriteLine("Promedio: {0}",promedio);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Guia_EjerciciosPractice/Ejercicio_02/Ejercicio_02/Ejercicio_02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 02";
+            int numero;
+            double cuadrado;
+            double cubo;
+
+            Console.Write("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            while(numero < 0)
+            {
+                Console.Write("ERROR. ¡Reingresar número!: ");
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            cuadrado = Math.Pow(numero, 2);
+            cubo = Math.Pow(numero, 3);
+
+            Console.WriteLine("El numero es: {0}, su cuadrado es: {1}, y el cubo es: {2}", numero, cuadrado, cubo);
+
+            Console.ReadKey();
+
+        }
+    }
+}
