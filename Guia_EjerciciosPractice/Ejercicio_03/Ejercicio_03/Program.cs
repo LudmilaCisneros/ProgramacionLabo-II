66614a0ae5506cc5a566efd84b2503b96d13c166
Ludmila Cisneros <ludmilacisneros97@hotmail.com>
2020-08-30 23:10:36 -0300

    ok

---
+++ Guia_EjerciciosPractice/Ejercicio_03/Ejercicio_03/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_03
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 03";
+            int numero;
+            bool EsPrimo = false;
+
+            Console.Write("Ingrese un numero: ");
+            while (!int.TryParse(Console.ReadLine(), out numero))
+            {
+                Console.Write("Error. Ingrese un numero: ");
+            }
+            while (numero <= 1)
+            {
+                Console.Write("Error. ingrese un numero mayor a 1: ");
+                int.TryParse(Console.ReadLine(), out numero);
+            }
+
+            for (int i=2; i <= numero; i++)
+            {
+                EsPrimo = SaberSiEsPrimo(i);
+                if (EsPrimo)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+
+            bool SaberSiEsPrimo(int num)
+            {
+                bool esPrimo = true;
+
+                for (int i = 2; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        esPrimo = false;
+                        break;
+                    }
+
+                }
+                return esPrimo;
+            }
+        }
+
+    }
+
+
+
+}
