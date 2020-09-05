bf76947e4b300895482f8a56020f631b14cd2462
Ludmila Cisneros <ludmilacisneros97@hotmail.com>
2020-09-05 19:45:07 -0300

    Revisar ejercicio 7

---
+++ Guia_EjerciciosPractice/Ejercicio_06/Ejercicio_06/Ejercicio_06/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+/*6. Escribir un programa que determine si un año es bisiesto.
+Un año es bisiesto si es múltiplo de 4. Los años múltiplos de 100 no son bisiestos, salvo si ellos
+también son múltiplos de 400. Por ejemplo: el año 2000 es bisiesto pero 1900 no.
+Pedirle al usuario un año de inicio y otro de fin y mostrar todos los años bisiestos en ese rango.
+Nota: Utilizar estructuras repetitivas, selectivas y la función módulo (%).*/
+
+namespace Ejercicio_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 06";
+            int hasta;
+            int desde;
+            
+            //Input datos
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write(".::AÑO BISIESTO::.\n");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("\nDesde: ");
+            while (!int.TryParse(Console.ReadLine(), out desde))
+            {
+                Console.Write("Error, reingrese: ");
+            }    
+
+            Console.Write("\nHasta: ");
+            while (!int.TryParse(Console.ReadLine(), out hasta))
+            {
+                Console.Write("Error, reingrese: ");
+            }
+            Console.Write("\n");
+            CalcularBisiestos(desde,hasta);   
+            Console.ReadKey();
+        }
+        public static void CalcularBisiestos(int desde, int hasta)
+        {
+            bool esBisiesto;
+
+            for (int i=desde; i<=hasta; i++)
+            {
+                esBisiesto = false;
+
+                if(i%4 == 0 || i%400 == 0)
+                {
+                    esBisiesto = true;
+                }
+                if(esBisiesto == true)
+                {
+                    Console.Write("{0}-",i);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Guia_EjerciciosPractice/Ejercicio_07/Ejercicio_07/Ejercicio_07/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 07";
+            int dia, mes, año, diasVividos;
+
+            //INPUT
+            /*
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("**::.¿CUÁNTOS AÑOS VIVISTE?.::**\n");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Ingresá tu fecha de nacimiento.<-");
+            Console.ResetColor();
+
+            Console.Write("Día: ");
+            while (!int.TryParse(Console.ReadLine(), out dia))
+            {
+                Console.Write("Error, reingrese: ");
+            }
+
+            Console.Write("Mes: ");
+            while (!int.TryParse(Console.ReadLine(), out mes))
+            {
+                Console.Write("Error, reingrese: ");
+            }
+
+            Console.Write("Año: ");
+            while (!int.TryParse(Console.ReadLine(), out año))
+            {
+                Console.Write("Error, reingrese: ");
+            }
+            */
+            dia = 12;
+            mes = 1;
+            año = 1997;
+            diasVividos = CalcularDias(dia, mes, año);
+
+            Console.WriteLine("-> Los días vividos son {0}", diasVividos);
+
+            Console.ReadKey();
+        }
+        public static bool EsBisiesto(int año)
+        {
+            bool respuesta = false;
+
+
+            if (año % 4 == 0 || año % 400 == 0)
+            {
+                respuesta = true;
+            }
+
+
+            return respuesta;
+        }
+        public static int CalcularDias(int dia, int mes, int año)
+        {
+            int contadorDias = 0;
+            int añoActual = (int)DateTime.Now.Year;
+            int mesActual = (int)DateTime.Now.Month;
+            int diaActual = (int)DateTime.Now.Day;
+
+            for (int i = año; i < añoActual; i++)
+            {
+                if (EsBisiesto(i))
+                {
+                    contadorDias += 366;
+                }
+                else
+                {
+                    contadorDias += 365;
+                }
+            }
+
+            for (int i = mes; i < mesActual; i++)
+            {
+                switch (mes)
+                {
+                    case 2:
+                        if (EsBisiesto(año))
+                        {
+                            contadorDias += 29;
+                        }
+                        else
+                        {
+                            contadorDias += 28;
+                        }
+                        break;
+
+                    case 1://ENERO
+                    case 3://MARZO
+                    case 5://MAYO
+                    case 7://JULIO
+                    case 8://AGOSTO
+                    case 10://OCTUBRE
+                    case 12://DICIEMBRE
+                        contadorDias += 31;
+                        break;
+
+                    case 4://ABRIL
+                    case 6://JUNIO
+                    case 9://SEPTIEMBRE
+                    case 11://NOVIEMBRE
+                        contadorDias += 30;
+                        break;
+
+                }
+
+            }
+            for (int i = 1; i < diaActual; i++)
+            {
+                contadorDias++;
+            }
+
+
+            return contadorDias;
+        }
+    }
+}
